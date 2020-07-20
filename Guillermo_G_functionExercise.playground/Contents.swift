b38ce894d5ff37def4f189629ae663bbe6e33063
GarciaGuillermo <67976128+GarciaGuillermo@users.noreply.github.com>
2020-07-20 14:15:57 -0500

    Add files via upload

---
+++ Guillermo_G_functionExercise.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Cocoa
+
+func addtwoints(a: Int , b: Int ) -> Int {
+    return a + b
+}
+
+var bounds = addtwoints(a: 7, b: 22)
+print ("the sum of my birthday month and day (a+b) is \(bounds)")
